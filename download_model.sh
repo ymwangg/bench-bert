4d839ef8e09cc98041fae518174b908d0d98efd7
Yanming Wang <yanmwang@amazon.com>
2021-06-22 18:43:28 +0000

    Minor change of download_model

--- download_model.sh
+++ download_model.sh
@@ -5,7 +5,7 @@ if [ -d models/$1 ]; then
     rm -rf $1
     cd ..
 fi
-mkdir models/$1
+mkdir -p models/$1
 cd models/$1
 python ${ROOTDIR}/convert_graph_to_onnx.py --framework pt --model $1 $1.onnx
 cd ${ROOTDIR}
